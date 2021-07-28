9e8590013d8cf4e8802beb901b1a256e847df3a4
EhWanu <cameronjshadgett@gmail.com>
2021-07-28 15:27:12 -0400

    synopsis button functioning

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -3,13 +3,14 @@ import { GetMoviesService } from '../fetch-api-data.service';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { MovieSynopsisComponent } from '../movie-synopsis/movie-synopsis.component';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss'],
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit {
   movies: any[] = [];
   constructor(
     public fetchApiData: GetMoviesService,
@@ -29,4 +30,11 @@ export class MovieCardComponent {
       return this.movies;
     });
   }
+
+  openSynopsisDialog(synopsis: string): void {
+    this.dialog.open(MovieSynopsisComponent, {
+      data: { synopsis },
+    });
+  }
+  
 }
